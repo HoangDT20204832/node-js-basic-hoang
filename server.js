da08a1f85e5ce92afd6a03693a06640827a6d0ee
HoangDT20204832 <108734115+HoangDT20204832@users.noreply.github.com>
2023-05-09 14:32:39 +0700

    video5

--- server.js
+++ server.js
@@ -2,15 +2,17 @@
 //npm install --save-exact express@4.17.1 : cài famework express phiên bản 4.17.1
 //môi 1 máy tính sinh ra 1 node_modules khác nhau nên ko nên share node_modules lên github (và nó cũng nặng)
 // => tạo thêm 1 file gitignore: file này giúp ta ghi những file nào ko muốn đẩy lên github
-const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2>from ERIC & HOI DAN IT </h2>');
-    res.end();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! with Hoang')
+})
+app.get('/hmm', (req, res) => {
+    res.send('Đâm nhau khum he')
 })
 
-server.listen(3000, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
