fdb843fbc5846b22eaf4a2d0930449d58c587689
HoangDT20204832 <108734115+HoangDT20204832@users.noreply.github.com>
2023-05-10 10:49:08 +0700

    video8

---
+++ src/route/web.js
@@ -0,0 +1,15 @@
+//phụ trách tất cả các route trên website của bạn
+import express from "express";
+import homeController from '../controller/homeController';
+let router = express.Router();
+
+const initWebRoute = (app) =>{
+    router.get('/', homeController.getHomepage);
+
+    router.get('/hmm', (req, res) => {
+        res.send('Đâm nhau khum he')
+    })
+    return app.use('/', router)//  /abc: thêm tiền tố trước mỗi row: vd:http://localhost:8080/abc/hmm thì mới chạy đc
+}
+
+export default initWebRoute;
--- src/server.js
+++ src/server.js
@@ -6,24 +6,25 @@
 //View engine: giúp xử lý các điều kiện logic trong file html ( vd như viết các vong lặp if else trong file html)
 // const express = require('express')
 //cấu hình file môi trường: npm install --save-exac dotenv@10.0.0
+//ko share file .env lên git
 //Static files: mục đích: chia sẻ dữ liệu trong file static ra ngoài cho người khác xem
+//MVC: 
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
 //để chạy câu lệnh process.env.PORT cần lệnh phía dưới
+import initWebRoute from './route/web';
 require('dotenv').config();
 const app = express();
 //muốn kết nối với database thì phải đưa các cổng,... vào file môi trường
 const port = process.env.PORT || 8080;
 console.log(port);
 
-configViewEngine(app)
-app.get('/', function(req, res) {
-  res.render('test/index.ejs');
-});
-app.get('/hmm', (req, res) => {
-  res.send('Đâm nhau khum he')
-})
+//setup viewEngine
+configViewEngine(app);
+//init web route
+initWebRoute(app);
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
